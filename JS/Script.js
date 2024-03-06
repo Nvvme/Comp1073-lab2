82949b57eb82655dde34c9c6128d2001be40493c
Nvvme <navan2400@gmail.com>
2024-03-06 11:54:37 -0500

    got it working

--- JS/Script.js
+++ JS/Script.js
@@ -12,6 +12,25 @@ function CrocRod(serialNumber, year, series, numberInSeries, color, tampo, windo
     this.wheelType = wheelType;
     this.baseColor = baseColor;
     this.manufacturingCountry = manufacturingCountry;
-  }
+}
 
+  // adding interactivity
+  
+  window.createCrocRod = function() {
+    var crocRod = new CrocRod('039/250', 2024, 'HW Mega Bite', '2/5', 'Blue', 'Orange and fuchsia graphics', 'Transparent orange', 'Plastic', 'None', 'Fuchsia RA6 Chrome', 'Fuchsia', 'Malaysia');
+    
+    // Display details
+    var details = document.getElementById('details');
+    details.innerHTML = `Serial Number: ${crocRod.serialNumber}<br>Year: ${crocRod.year}<br>Series: ${crocRod.series}<br>.. and so on`;
+  };
 
+  // set background image
+
+  document.getElementById('setBackgroundButton').addEventListener('click', function() {
+    document.body.style.backgroundImage = "url('./Images/Croc_Rod_HCT02.webp')";
+    document.body.style.backgroundSize = 'sticky';
+    document.body.style.backgroundRepeat = 'no-repeat';
+    document.body.style. backgroundPositionY = '500px';
+    document.body.style.backgroundPosition = 'center';
+  });
+  
