283b6101a90d374475b2f0e80957a9034dbfad45
Nvvme <navan2400@gmail.com>
2024-03-06 11:32:38 -0500

    added the JS and some CSS

---
+++ JS/Script.js
@@ -0,0 +1,17 @@
+// added the object constructor and the properties
+function CrocRod(serialNumber, year, series, numberInSeries, color, tampo, windowColor, bodyMaterial, interior, wheelType, baseColor, manufacturingCountry) {
+    this.serialNumber = serialNumber;
+    this.year = year;
+    this.series = series;
+    this.numberInSeries = numberInSeries;
+    this.color = color;
+    this.tampo = tampo;
+    this.windowColor = windowColor;
+    this.bodyMaterial = bodyMaterial;
+    this.interior = interior;
+    this.wheelType = wheelType;
+    this.baseColor = baseColor;
+    this.manufacturingCountry = manufacturingCountry;
+  }
+
+
